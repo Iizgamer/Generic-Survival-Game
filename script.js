2d8a6a96e0c7dc149edfe792cce046aa5c46ffd7
Nicholas Foles _ Student - WakeIBTechEC <npfoles@students.wcpss.net>
2026-04-08 09:42:39 -0400

    mmmm

---
+++ script.js
@@ -0,0 +1,4 @@
+const titleText = document.getElementById('titleText');
+const statusText = document.getElementById('statusText');
+
+statusText.textContent = 'You woke up early in the morning on the ship, the rays of sunlight from the early morning sunrise sneaking through your closed blinds. Finally, you were escaping your war-ridden country, immigrating to a nation that would let you be free. You can feel the rough waves rocking the ship lightly, causing you to stumble slightly, but you manage to stay on your feet.'
