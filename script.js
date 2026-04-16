591704781f0c053c265c5f6ac19c38776c0d627f
Nicholas Foles _ Student - WakeIBTechEC <npfoles@students.wcpss.net>
2026-04-16 08:45:50 -0400

    dududud

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const titleText = document.getElementById('titleText');
 const statusText = document.getElementById('statusText');
+const continueTextBtn = document.getElementById('continueTextBtn')
+let continueTextBtnCount = 0
 
-statusText.textContent = 'You woke up early in the morning on the ship, the rays of sunlight from the early morning sunrise sneaking through your closed blinds. Finally, you were escaping your war-ridden country, immigrating to a nation that would let you be free. You can feel the rough waves rocking the ship lightly, causing you to stumble slightly, but you manage to stay on your feet.'
+
+statusText.textContent = 'You woke up early in the morning on the ship, the rays of sunlight from the early morning sunrise sneaking through your closed blinds. You can feel the rough waves rocking the ship lightly, causing you to stumble slightly, but you manage to stay on your feet.'
+
+continueTextBtn.addEventListener('click', function(){
+    continueTextBtnCount++
+    if (continueTextBtnCount == 1) {
+        statusText.textContent = 'Then, the sound you were dreading the most creeps — or, more accurately — invades your ears.'
+    }
+})
